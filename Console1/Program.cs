b7da0589a984496fd00bdc59a2afc35d535397b3
Tenen-D <cleverain@ya.ru>
2017-03-15 00:40:11 +0200

    C5

--- Console1/Program.cs
+++ Console1/Program.cs
@@ -15,15 +15,17 @@ namespace Console1
             Console.WriteLine("******Abra kadabra******");
             Console.WriteLine("Privet MIR");
             Console.WriteLine();
-
+            //                          ЧАСТЬ1
             // GetUserData();           // В этом методе програма использует данные введеные пользователем
             // MessBox();               // В этом методе открывается отдельное окно с надписью
             // TipesNew ();             // Метод в котором создаются типы переменних с помощю оператора "NEW"  со стандартным значением
             // TimeAndWatch();          // Операторы времени
             // StringMutki();           // Методы работы с текстом
             // StringBuilderMutki();    // StringBuilder более ресурсо-економный нежели string  
-            nnn();
+            // Tsikli();                // Тут описаны 4 вида циклов (for, foreach, while, do/while)
+            OperVarianti();          // Операторы (if/else и switch) - Операторы ветвления ("возможные варианты поведения кода") 
 
+            Console.WriteLine("exit");
 
             Console.WriteLine();
 
@@ -123,14 +125,125 @@ namespace Console1
             Console.WriteLine("Всего в песенке {0} букв.", _s.Length);
             Console.WriteLine();
             _s.Replace("арусель", "окаин");                                              // (Replace) Заменяет все "арусель" на "окаин"
-            _s.Replace("Прокатись на нашей карусели", "Занюхни ка вмести с нами");
+            _s.Replace("Прокатись на нашей карусели", "Занюхни ка вместе с нами");
             Console.WriteLine(_s.ToString());
             Console.WriteLine("Длинна песенки {0} символов", _s.Length);
         }
-        static void nnn ()
+        static void Tsikli()
         {
+
+            Console.WriteLine("\t\tЦикл for ");
+            Console.WriteLine("Введи цыфру, и полуишь ее квадрат и куб");
+           
+            string o = Console.ReadLine();
+            int z = int.Parse(o);
+            int p = z;
+            Console.WriteLine("цыфра\tквадрат\tкуб");
+            Console.Write(p);
+            for (int i = 0; i < 2; i++)
+            {
+                p = z * p;
+                Console.Write("\t{0}",p);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            //////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            Console.WriteLine("\t\tЦикл foreach");
+            string[] ii = { "камень", "вино","дерево", "метал" }; //Создаем масив
+            Console.WriteLine("{0} {1} {2} {3}",ii);
             
+            foreach (string m in ii)                                     
+            {
+               
+                Console.WriteLine(m);                               //Выводим поочередно значения масива на экран
+            }
+            Console.WriteLine();
+            Console.WriteLine(); 
+            //////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            Console.WriteLine("\t\tЦикл while");
+            bool n = false;
+            int b = 0;
+            string a = "password";
+            while(n == false || b < 1)                          //Цикл будет повторятся пока не будет нарушено одно из условий
+            {
+                Console.WriteLine("Vvedite password");
+                string m = Console.ReadLine();
+                if (m == a)
+                {
+                    n = true;
+                    b += 1;
+                }
+                else
+                {
+                    Console.WriteLine("Oshibka");
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            Console.WriteLine("\t\tНиже представлен цикл do/while, котороый розоблачает тебя");
+            string mm;
+            do
+            {
+                Console.WriteLine("You a gay?");
+                 mm = Console.ReadLine();
+            }
+            while( mm != "yes");
+            
+            Console.WriteLine("ха ха ха");
         }
-        
+        static void OperVarianti()
+        {
+            //Оператор if/else
+            V1:
+            Console.WriteLine("Ты черепашка ниндзя?\ny-да \nn-нет");
+            string otvet = Console.ReadLine();
+            if(otvet == "y")
+            {
+                Console.WriteLine("Вот и молодец");
+            }
+            else if (otvet =="n")
+            {
+                Console.WriteLine("А, ты и з этих, телепузиков...");
+            }
+            else
+            {
+                Console.WriteLine("Ты думаеш я тут шутки шучу? Отвечай на вопрос!");
+                Console.WriteLine();
+                goto V1;
+            }
+            Console.WriteLine();
+
+            // Оператор switch
+            PoftorVoprosa:
+            ConsoleColor defolt_Color = Console.ForegroundColor;        //запоминаем изначальный цвет текста
+            Console.WriteLine("синий, желтый или зеленый?");
+            string q = Console.ReadLine();
+            switch (q)
+            {
+                case "синий":
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    break;
+                case "желтый":
+
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                        break;
+                case "зеленый":
+                    
+                    Console.ForegroundColor = ConsoleColor.Green;
+                        break;
+                default:
+                    Console.WriteLine("Тебе задали вопрос!!!");
+                    goto PoftorVoprosa;                    
+            }
+            Console.WriteLine("Хорошый цвет, мне нравится");
+            Console.ForegroundColor = defolt_Color ;                         //возвращяем изначальный цвет текста
+        }        
     }
 }
